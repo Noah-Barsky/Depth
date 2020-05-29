d733e052e0fa20437e524b0b238f21c186b94fe5
Noah Barsky <noahbarsky1@gmail.com>
2020-05-30 00:49:34 +0800

    Grows Barren Saplings In Biome

--- Tiles/BarrenBiome/BarrenDirtTile.cs
+++ Tiles/BarrenBiome/BarrenDirtTile.cs
@@ -2,24 +2,22 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace Depth.Tiles.BarrenBiome
-{
-    public class BarrenDirtTile : ModTile
-    {
-        public override void SetDefaults()
-        {
+namespace Depth.Tiles.BarrenBiome {
+    public class BarrenDirtTile : ModTile {
+        public override void SetDefaults() {
             Main.tileSolid[Type] = true;
             Main.tileMergeDirt[Type] = false;
-            Main.tileBlockLight[Type] = false;  
+            Main.tileBlockLight[Type] = false;
             Main.tileLighted[Type] = false;
-           // Main.tileMerge[Type][mod.TileType("AuroraStoneTile")] = true;
-            drop = mod.ItemType("BarrenDirt");  
+            // Main.tileMerge[Type][mod.TileType("AuroraStoneTile")] = true;
+            drop = mod.ItemType("BarrenDirt");
             AddMapEntry(new Color(15, 61, 61));
+            SetModTree(new Tree.BarrenthornTree());
         }
 
         public override int SaplingGrowthType(ref int style) {
-			style = 0;
-			return mod.TileType("BarrenthornSapling");
+            style = 0;
+            return mod.TileType("BarrenthornSapling");
 
         }
     }
