4ed235cda5627d07db95e1c68e7be1479da84f78
Kiddoops <46802978+Kiddoops@users.noreply.github.com>
2020-05-29 06:10:13 -0500

    Add files via upload

---
+++ DepthPlayer.cs
@@ -0,0 +1,23 @@
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.Localization;
+using Terraria.Graphics.Shaders;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.UI;
+using Depth;
+
+namespace Depth
+{
+    public class DepthPlayer : ModPlayer
+    {
+        public bool GenBarren;
+
+
+    }
+}
