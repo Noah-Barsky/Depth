0c61baaf7bc830e0c5488772b2064b0eb8449f69
Kiddoops <46802978+Kiddoops@users.noreply.github.com>
2020-06-09 18:54:04 -0500

    Update SoulBuff.cs

--- Buffs/SoulBuff.cs
+++ Buffs/SoulBuff.cs
@@ -12,9 +12,9 @@ namespace Depth.Buffs {
         }
 
         public override void Update(Player player, ref int buffIndex) {
-            bool petProjectileNotSpawned = player.ownedProjectileCounts[ProjectileType<Projectiles.PlasmaBall>()] <= 0;
+            bool petProjectileNotSpawned = player.ownedProjectileCounts[ProjectileType<Projectiles.DarkmancerSpirit>()] <= 0;
             if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer) {
-                Main.LocalPlayer.GetModPlayer<DepthPlayer>().soulProjectile = Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ProjectileType<Projectiles.PlasmaBall>(), 5, 0f, player.whoAmI, 0f, 0f);
+                Main.LocalPlayer.GetModPlayer<DepthPlayer>().soulProjectile = Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ProjectileType<Projectiles.DarkmancerSpirit>(), 5, 0f, player.whoAmI, 0f, 0f);
             }
         }
     }
